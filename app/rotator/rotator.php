6982e3dc4af60aa98a7f8a2ce5cec4f61d6b71aa
motto <motto001@gmail.com>
2016-02-26 00:07:30 +0100

    rotator infoablak ok!

--- app/rotator/rotator.php
+++ app/rotator/rotator.php
@@ -5,24 +5,114 @@ class ADT
     public static $jog='user';
     public static $itemidtomb=array();
     public static $allowed_func=array('uj','vissza','ment','joghiba');
+
 }
 $mktime_most=time();
-$mktime=$mktime_most-6000;
+$mktime=$mktime_most-600;// max10 órával ezelőttieket nézi csak
 $sql="SELECT * FROM faucet ORDER BY pont DESC,perc DESC ";
 $linktomb=DB::assoc_tomb($sql);
-$sql="SELECT * FROM faucet_log WHERE userid ='".$_SESSION['userid']."' AND mktime>".$mktime."";
+
+$sql="SELECT * FROM faucet_log WHERE userid ='".$_SESSION['userid']."' AND mktime>'".$mktime."' ORDER BY mktime ";
 $logtomb=DB::assoc_tomb($sql);
-if(is_array($logtomb))
+
+$logtomb_idkulcs=TOMB::mezobol_kulcs($logtomb,'linkid');
+//print_r($logtomb_idkulcs);
+$akt_link=array();
+$varotomb=array();
+if(empty($logtomb))
 {
-  $lik= $linktomb[0] ;
+    $akt_link= $linktomb[0] ;
 }
 else
 {
   foreach($linktomb as $link)
   {
+      $linkid=$link['id'];
+      if(empty($akt_link))
+      {
+        if(isset($logtomb_idkulcs[$linkid]))
+        {
+         $logsor=$logtomb_idkulcs[$linkid];
+         $lejar=$logsor['mktime']+$link['perc'];//másodpercet számol
+            //$lejar=$logsor['mktime']+$link['perc']*60; //percet számol
+             if($lejar<$mktime_most)
+             {
+                 $akt_link=$link;
+             }
+             else
+             {$hatravan=$lejar-$mktime_most;
+                 $link['lejar']=$hatravan;
+                 $varotomb[]=$link;
+             }
 
+        }
+        else
+        {
+          $akt_link=$link;
+        }
+      }
   }
+
 }
-$tartalom = file_get_contents('tmpl/'.GOB::$tmpl.'/alap.html', true);
-$tartalom = str_replace('<!--|tartalom|-->', '', $tartalom);
-echo $tartalom;
+$varolista='';
+function varolink($link,$pont,$perc)
+{
+   $result='<div class="varodiv">
+                <div class="varolink">'.parse_url($link, PHP_URL_HOST).'</div>
+                <div class="varopont">'.$pont.'</div>
+                <div class="varoperc">'.$perc.'</div>
+                <div style="clear: both;"></div>
+             </div>' ;
+    return $result;
+}
+$varolista='<style>
+.varolink{
+float: left;
+background-color:white ;
+width: 150px;
+color: #080808;
+padding: 2px;
+margin: 1px;
+overflow: hidden;
+}
+.varopont{
+float: left;
+background-color:white ;
+width: 40px;
+color: #080808;
+padding: 2px;
+margin: 1px;
+}
+.varoperc{
+float: left;
+background-color:red ;
+color: yellow;
+width: 40px;
+padding: 2px;
+margin: 1px;
+
+}
+</style>';
+$varolista= $varolista.'<div id="infoablak" style="position:fixed;top:100px;left:10px;vidth:1000px;z-index: 1000;">';
+$varolista= $varolista.'<div class="varodiv">
+                <div class="varolink">Link</div>
+                <div class="varopont">Pont</div>
+                <div class="varopont">perc</div>
+                <div style="clear: both;"></div>
+             </div>' ;
+foreach($varotomb as $varosor)
+{
+  $varolista= $varolista.varolink($varosor['link'],$varosor['pont'],$varosor['lejar']);
+}
+$varolista= $varolista.'</div>';
+//if(empty($akt_link))
+$sql="INSERT INTO faucet_log (userid,linkid,mktime)
+VALUES ('".$_SESSION['userid']."','".$akt_link['id']."','".$mktime_most."')";
+DB::beszur($sql);
+$tartalom='<h5>link</h5>';
+$tartalom=$tartalom.TOMB::to_string($akt_link);
+$tartalom=$tartalom.'<h5>varotomb</h5>';
+$tartalom=$tartalom.$varolista;
+$view = file_get_contents('tmpl/'.GOB::$tmpl.'/alap.html', true);
+$view = str_replace('<!--|tartalom|-->', $tartalom,$view );
+echo $view;
--- lib/altalanos_fgv.php
+++ lib/altalanos_fgv.php
@@ -33,10 +33,35 @@ static public function webnev($string,$hosz=20)
 }
 
 class TOMB {
-static public function to_string($tomb){
-	$str='';
-foreach($tomb as $key=>$value){
-if(is_array($value)){self::to_string($value);}else{$str=$str. $key.': '.$value.'\n </br>';}}
+	/**
+	 * ['id'=>'user1','nev'=>'otto']
+	 * ból:[user1=>['id'=>'user1','nev'=>'otto']
+	 * a kulcsmező értékét kiemeli sor kulcsnak;
+	 * ha több egyforma érték is van, felülírja az ORDER BY-nak megfelellően
+	 */
+	static public function mezobol_kulcs(array $tomb, $kulcsmezo='id'){
+		$tomb2=array();
+		foreach($tomb as $sor )
+		{	$sorindex= $sor[$kulcsmezo];
+			$tomb2[$sorindex]=$sor;
+		}
+
+		return $tomb2;
+	}
+static public function to_string($tomb)
+{
+	$str = '';
+	foreach ($tomb as $key => $value)
+	{
+		if (is_array($value))
+		{
+			$str =$str.self::to_string($value);
+		}
+		else
+		{
+			$str = $str . $key . ': ' . $value . '\n </br>';
+		}
+	}
 	return $str;
 }
 static public function kiir($tomb){
